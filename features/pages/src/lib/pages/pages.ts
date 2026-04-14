89fcdc2a794d7b761a9be2c86ae87449b58dec82
Youssef <57530419+iYoussefJoey@users.noreply.github.com>
2026-04-14 15:05:22 +0200

    feature/created nx project

---
+++ features/pages/src/lib/pages/pages.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-pages',
+  imports: [],
+  templateUrl: './pages.html',
+  styleUrl: './pages.css',
+})
+export class Pages {}

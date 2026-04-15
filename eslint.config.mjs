39553aa143839a0a69c6ded497c9acad8cc7eb95
Youssef <57530419+iYoussefJoey@users.noreply.github.com>
2026-04-15 10:09:26 +0200

    feat(eslint): implemented a new rules for TS to make it more strict

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import nx from '@nx/eslint-plugin';
-
+import jsdoc from 'eslint-plugin-jsdoc';
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
@@ -36,7 +36,50 @@ export default [
       '**/*.cjs',
       '**/*.mjs',
     ],
-    // Override or add rules here
-    rules: {},
+    plugins: {
+      jsdoc,
+    },
+    rules: {
+      'jsdoc/require-jsdoc': [
+        'error',
+        {
+          publicOnly: false,
+          contexts: [
+            'ClassDeclaration',
+            'FunctionDeclaration',
+            'MethodDefinition',
+            'ArrowFunctionExpression',
+            'FunctionExpression',
+          ],
+          require: {
+            ClassDeclaration: true,
+            FunctionDeclaration: true,
+            MethodDefinition: true,
+            ArrowFunctionExpression: true,
+            FunctionExpression: true,
+          },
+        },
+      ],
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: false,
+          allowTypedFunctionExpressions: false,
+          allowHigherOrderFunctions: false,
+        },
+      ],
+      'jsdoc/require-param': 'error',
+      'jsdoc/require-returns': 'error',
+      'jsdoc/require-returns-check': 'error',
+    },
+  },
+  {
+    files: ['**/*.spec.ts', '**/*.test.ts', '**/*.test-setup.ts'],
+    rules: {
+      'jsdoc/require-jsdoc': 'off',
+      'jsdoc/require-description': 'off',
+      'jsdoc/require-param': 'off',
+      'jsdoc/require-returns': 'off',
+    },
   },
 ];
